3c6bfecd708584383dada3288f75ef60738e9d91
Aleksandr Gonchar <shuric2005@mail.ru>
2021-04-11 18:24:46 +0300

    added bundles

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    AlexGoncharCK\ChainCommandBundle\ChainCommandBundle::class => ['all' => true],
+    AlexGoncharCK\BarBundle\BarBundle::class => ['all' => true],
+    AlexGoncharCK\FooBundle\FooBundle::class => ['all' => true],
 ];
